c4d031af714c1e6cf2195100cea0952c7dff2572
Hanna Mulugeta <Hannabmulugeta@gmail.com>
2020-11-18 14:17:21 -0500

    Update questions.sql
    Using Date filtering to sort datas.

--- SQL Deep Dive/Date Filtering/questions.sql
+++ SQL Deep Dive/Date Filtering/questions.sql
@@ -4,7 +4,9 @@
 * Question: Get me all the employees above 60, use the appropriate date functions
 */
 
-SELECT * FROM employees;
+select emp_no, first_name, last_name, gender, hire_date, age( birth_date) AS age
+from employees
+where Extract(year from age(birth_date )) >60;
 
 /*
 * DB: Employees
@@ -12,7 +14,9 @@ SELECT * FROM employees;
 * Question: How many employees where hired in February?
 */
 
-SELECT * FROM employees;
+select count(first_name)
+from employees
+where extract (month from hire_date) = 2
 
 /*
 * DB: Employees
@@ -20,7 +24,9 @@ SELECT * FROM employees;
 * Question: How many employees were born in november?
 */
 
-SELECT * FROM employees;
+select count(first_name)
+from employees
+where extract (month from birth_date) = 11;
 
 /*
 * DB: Employees
@@ -28,7 +34,8 @@ SELECT * FROM employees;
 * Question: Who is the oldest employee? (Use the analytical function MAX)
 */
 
-SELECT * FROM employees;
+select max(age(birth_date))
+from employees;
 
 /*
 * DB: Store
@@ -36,5 +43,7 @@ SELECT * FROM employees;
 * Question: How many orders were made in January 2004?
 */
 
-SELECT * FROM orders;
+select count(orderid)
+from orders
+where date_trunc('month', orderdate) = '2004-01-01'
 
