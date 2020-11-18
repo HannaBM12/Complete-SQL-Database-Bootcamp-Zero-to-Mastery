6fc3e8c07f1b3fa659c8cac3677fd1ecb94c4258
Hanna Mulugeta <Hannabmulugeta@gmail.com>
2020-11-18 00:02:20 -0500

    Update questions.sql
    Partial filtering data with LIKE and ILIKE.

--- SQL Deep Dive/Like Operator/questions.sql
+++ SQL Deep Dive/Like Operator/questions.sql
@@ -5,7 +5,8 @@
 * Sample output: https://imgur.com/vXs4093
 * Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
 */
-SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
+SELECT emp_no, first_name, EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees
+Where first_name like 'M%';
 
 
 /*
@@ -14,7 +15,8 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's name start with A and end with R?
 * Expected output: 1846
 */
-
+SELECT count(emp_no) FROM employees
+Where first_name Ilike 'A%R';
                                                   
 /*
 * DB: Store
@@ -22,7 +24,10 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode have a 2 in it?.
 * Expected output: 4211 
 */
-
+                                                  
+SELECT count(firstname) FROM customers
+Where zip::text Ilike '%2%';
+                                                  
 
 
 /*
@@ -32,6 +37,8 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Expected output: 109 
 */
 
+SELECT count(firstname) FROM customers
+Where zip::text Ilike '2_1%';
 
 /*
 * DB: Store
@@ -40,4 +47,5 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Replace null values with "No State"                                                  
 * Expected output: https://imgur.com/AVe6G4c
 */
-
+SELECT coalesce (state, 'No State')as State FROM customers
+Where phone::text like '302%';
